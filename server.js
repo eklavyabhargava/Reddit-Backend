b8357010b12bbb895412440ddea0f7a4e16681ec
Eklavya Bhargava <eklavyabhargawa@gmail.com>
2024-11-16 20:26:56 +0530

    trust proxy 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDb();
 require("./models/User.js");
 require("./models/Post.js");
 
-app.set("trust proxy", 1);
+app.set("trust proxy", 2);
 
 app.use((req, res, next) => {
   console.log("Client IP:", req.ip);
